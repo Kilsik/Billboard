1fc3ef89d38c293d9da5e20a1b7e93e419a077f3
Kilsik <ilsiwow@gmail.com>
2023-06-09 17:59:44 +0300

    Add images in Places admin

--- places/admin.py
+++ places/admin.py
@@ -3,10 +3,15 @@ from django.contrib import admin
 from .models import Places, Images
 
 
+class ImagesInline(admin.TabularInline):
+    model = Images
+
+
 @admin.register(Images)
 class ImagesAdmin(admin.ModelAdmin):
     list_display = ["pk", "place"]
     ordering = ('pk',)
 
-admin.site.register(Places)
-# admin.site.register(Images)
+@admin.register(Places)
+class PlacesAdmin(admin.ModelAdmin):
+    inlines = [ImagesInline]
